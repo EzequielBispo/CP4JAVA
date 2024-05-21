17e8b2fc1160d5dba1dd553c193c950d212838bb
Ezequiel Bispo <ezequielbispo011@gmail.com>
2024-05-21 19:34:16 -0300

    Commit inicial.

---
+++ src/main/java/com/emi/cp3java/Cp3JavaApplication.java
@@ -0,0 +1,13 @@
+package com.emi.cp3java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp3JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cp3JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emi/cp3java/Cp3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emi.cp3java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp3JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
